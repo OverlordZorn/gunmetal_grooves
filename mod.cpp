85c3220b1cc015c1708316ea9930416fc54cc78a
OverlordZorn <dennisAzorn@gmail.com>
2026-03-18 14:18:02 +0100

    fix logo

--- mod.cpp
+++ mod.cpp
@@ -2,13 +2,13 @@ name = "Gunmetal Grooves";
 
 author = "Lucid Vision Studios";
 
-picture         = "extras\gg_logo.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
+picture         = "gg_logo.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
 hideName        = "false";                                                                                                                          // Hide the extension name
 hidePicture     = "false";                                                                                                                          // Hide the extension menu
 
-logoSmall       = "extras\gg_logo.paa";                                                                                                    // Display next to the item added by the mod 64x64
-logo            = "extras\gg_logo.paa";                                                                                                   // Logo displayed in the main menu 128x128
-logoOver        = "extras\gg_logo.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
+logoSmall       = "gg_logo.paa";                                                                                                    // Display next to the item added by the mod 64x64
+logo            = "gg_logo.paa";                                                                                                   // Logo displayed in the main menu 128x128
+logoOver        = "gg_logo.paa";                                                                                                     // When the mouse is over, in the main menu 128x128
 
 tooltip         = "tooltip - If you read this, someone forgot to replace a string in the mod.cpp";
 tooltipOwned    = "tooltipOwned";                                                                                                          // Tool tip displayed when the mouse is left over, in the main menu
@@ -17,7 +17,7 @@ description     = "description";                                                
 action          = "https://discord.gg/V37bRTnbb8/";                                                                                                // Website URL, that can accessed from the expansions menu 
 actionName      = "Discord";                                                                                                                        // label of button/tooltip in extension menu
 
-overviewPicture = "extras\gg_logo.paa";
+overviewPicture = "gg_logo.paa";
 overviewText = "overviewText";
 
 overviewFootnote = "overviewFootnote";
